29493effa8bd48eb4b40715b9f2ec6ed0b624562
wanjiSHACKS <wanjiciku36@gmail.com>
2026-07-14 23:51:44 +0300

    another one.

--- script.js
+++
@@ -1,6 +0,0 @@
-function showTime() {
-                const timeNow = new Date().toLocateTimeString();
-                document.getElementById('clock').textContent = timeNow;
-            }
-
-            setInterval(showTime, 1000);
